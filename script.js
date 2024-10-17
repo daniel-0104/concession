59607fe60b554a91f7acdc3b4d16fd67c561b15c
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-10-18 00:05:46 +0630

    18 night commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// href clicked active link start 
+document.addEventListener('DOMContentLoaded', () => {
+    const currentHTMLPage = window.location.pathname;
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => link.classList.remove('active'));
+  
+    navLinks.forEach(link => {
+      const linkPath = new URL(link.href).pathname;
+      if (currentHTMLPage === linkPath) {
+        link.classList.add('active');
+      }
+    });
+});
+// href clicked active link end
+
+
+// ...................................................... combo swiper start .................................
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  slidesPerView: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    519: {
+      slidesPerView: 2,
+    },
+    694: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+    },
+    963: {
+      slidesPerView: 4,
+    },
+    1400: {
+      slidesPerView: 5,
+    }                          
+  }
+});
+// ..................................................... combo swiper end ....................................
